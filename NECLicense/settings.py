eb12287785b499929fca3828892a134dd390f358
Bhoj Raj Bist <bhojbist1@gmail.com>
2024-07-23 21:43:10 +0545

    nbj

--- NECLicense/settings.py
+++ NECLicense/settings.py
@@ -160,11 +160,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # URL prefix for static files
 STATIC_URL = '/static/'
-
-# Static files directories (optional)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
